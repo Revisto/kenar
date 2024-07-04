aa4d45a66e1ea15dfe3938c33a369f97c180aeb7
Revisto <theRevisto@gmail.com>
2024-07-04 14:08:09 +0330

    chore: Update port number in main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ db.init_app(app)
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=8000)
