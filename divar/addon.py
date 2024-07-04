d9cf51523a81c297d9e21326b63de38491237225
Revisto <theRevisto@gmail.com>
2024-07-04 15:10:44 +0330

    feat: Update addon.py to display a 3D file with a custom label and link

--- divar/addon.py
+++ divar/addon.py
@@ -27,17 +27,17 @@ POST_TOKEN = "gZFmMQKr"
 ACCESS_TOKEN_HERE = "ory_at__ywH41CL1uzWZP67knIpkp9f3x6gq17HXBVmBcWM2gw.Lig8Btar7scjNHGjXbW0DvDjCWMk3tSA1Qwyj34fOo0"
 
 if __name__ == "__main__":
-    rsp = app.addon.upload_image("sofa.png")
+    rsp = app.addon.upload_image("logo.png")
     image_name = rsp.image_name
 
     event_row = EventRow(
-        title="تایتل",
-        subtitle="سابتایتل",
+        title="سه بعدی",
+        subtitle="فایل سه‌بعدی‌شو ببین",
         has_indicator=False,
         image_url=image_name,
-        label="لیبل",
+        label="3d",
         has_divider=True,
-        link="https://www.azibom.com/",
+        link=f"https://divar.azibom.com/?action=view&post_token={POST_TOKEN}",
         padded=True,
         icon=Icon(icon_name=IconName.ADD),
     )
